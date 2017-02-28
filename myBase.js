b3ede623139940d7592545dc276e017c14958156
hanekaoru <vd2o@foxmail.com>
2017-02-28 18:21:33 +0800

    update

--- myBase.js
+++ myBase.js
@@ -15,7 +15,10 @@
 
 var myBase = {
 
+
+
     // ------------------------ 常用方法 ---------------------------------//
+
 
 
     /* tab 切换 类似 jQuery 的 siblings() 方法 【 obj 为父级元素 】 */
@@ -25,23 +28,11 @@ var myBase = {
                 for (var i = 0; i < obj.length; i++) {
                     obj[i].className = "";
                 }
-                this.className = "active"
+                obj.className = "active"
             }
         }
     },
 
-    /* 查询字符串的 code 值 【 name 需要查询的字符串 】 */
-    getParameterByName: function (name) {
-
-        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-
-        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-
-            results = regex.exec(location.search);
-
-        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-
-    },
 
     /* 解析 url 为字典对象 【 getQueryObject(url).userId 】 */
     getQueryObject: function (url) {
@@ -68,6 +59,7 @@ var myBase = {
 
     },
 
+
     /* 保存 / 读取 key（备用） 【 saveKey() 】 */
     saveKey: function () {
         /*
@@ -85,6 +77,7 @@ var myBase = {
         }
         */
     },
+
 
     /* 随机验证码 【 createCode() 】 */
     createCode: function () {
@@ -109,6 +102,7 @@ var myBase = {
         checkCode.value = code;
 
     },
+
 
     /* 导航 下拉隐藏 上拉显示 【 scrollHide($("#header")) 】 */
     scrollHide: function (obj) {
@@ -170,10 +164,11 @@ var myBase = {
 
     },
 
+
     /* 多少秒后自动跳转（内部需要获取 id） 【 jumpTo(60, "url地址") 】 */
     jumpTo: function (secs, url) {
         
-        var obj = document.getElementById("aa");
+        var obj = document.getElementById("obj");
 
         obj.innerHTML = secs + "秒后自动跳转……";
 
@@ -187,6 +182,36 @@ var myBase = {
         }
 
     },
+
+
+    /* iframe 高度自适应 【 <iframe id="myFrameId" onload="iframeHeight()"></iframe> 】 */
+    iframeHeight: function () {
+        
+        var ifm = document.getElementById("myFrameId");
+        var subWeb = document.frames ? document.frames["myFrameId"].document : ifm.contentDocument;
+
+        if (ifm != null && subWeb != null) {
+            ifm.height = subWeb.body.scrollHeight;
+            ifm.width = subWeb.body.scrollWidth;
+        }
+
+    },
+
+
+    /* 获取一个随机的颜色 【 randColor() 】 */
+    randColor: function () {
+        
+        return "rgb(" + Math.ceil(255 * Math.random()) + "," + Math.ceil(255 * Math.random()) + "," + Math.ceil(255 * Math.random()) + ")";
+
+    },
+
+
+
+
+    // ------------------------ 判断相关 ---------------------------------//
+
+
+
 
     /* 判断上传文件大小及类型 【 CheckFile("file元素") 】 */
     jumpTo: function (obj) {
@@ -239,7 +264,202 @@ var myBase = {
     },
 
 
+
+
+    // ------------------------ 移动端相关 ---------------------------------//
+
+
+
+
+    /* 滑动底部加载更多 【 ... 】 */
+    loadMore: function () {
+        
+        $(window).scroll(function () {
+            if ($(this).scrollTop() + $(window).height() >= $(document).height() && $(this).scrollTop()) {
+                $.ajax({
+                    url: url,
+                    type: "get",
+                    success: function (data) {
+                        // ...
+                    }
+                })
+            }
+        });
+
+    },
+
+
+    /* 遮罩层下禁止滑动 【 禁止滑动 】 */
+    addBan: function () {
+        $("body").bind("touchmove", function (event) { event.preventDefault() });
+    },
+
+
+    /* 遮罩层下开启滑动 【 开启滑动 】 */
+    removeBan: function () {
+        $("body").unbind("touchmove");
+    },
+
+
+    /* 设置页面 rem 比例 【 rem 】 */
+    htmlFontSize: function () {
+        var clientWidth = document.documentElement ? document.documentElement.clientWidth : document.body.clientWidth;
+
+        if (clientWidth > 640) clientWidth = 640;
+
+        document.documentElement.style.fontSize = clientWidth * 1 / 16 + "px";
+
+        return clientWidth * 1 / 16;
+    },
+
+
+
+
+    // ------------------------ 字符串相关 ---------------------------------//
+
+
+
+
+    /* 查询字符串的 code 值 【 name 需要查询的字符串 】 */
+    getParameterByName: function (name) {
+
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+
+            results = regex.exec(location.search);
+
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+
+    },
+
+
+
+
+    // ------------------------ 日历相关 ---------------------------------//
+
+
+
+
+    /* 获取当前时间 【 getTime() 】*/
+    getTime: function () {
+
+        var date = new Date();
+
+        var y = date.getFullYear();
+        var m = date.getMonth() + 1;
+        var d = date.getDate();
+        var h = date.getHours();
+        var mi = date.getMinutes();
+
+        m = (m < 10) ? '0' + m : m;
+        d = (d < 10) ? '0' + d : d;
+        h = (h < 10) ? '0' + h : h;
+        mi = (mi < 10) ? '0' + mi : mi;
+
+        return y + '-' + m + '-' + d + ' ' + h + ':' + mi;
+    },
+
+
+    /* 序列化时间戳 【 timestamp(1487904182000) 】*/
+    timestampList: function (time) {
+
+        var date = new Date(time);
+
+        Y = date.getFullYear() + '-';
+        M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+        D = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+
+        h = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+        m = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+        
+        return (Y + M + '-' + D + ' ' + h + ':' + m);
+
+    },
+
+
+    /* 日历中参数补零 【 zeroFill(date) => 9 => 09年/月 】*/
+    zeroFill: function (x) {
+        return x = x < 10 ? "0" + x : x;
+    },
+
+
+    /* 时间间隔天数计算 【 DateDiff(s1, s2) 】*/
+    dateDiff: function (sDate1, sDate2) {
+
+        // s1 = "2017-09-30"
+        // s2 = "2017-10-08"
+
+        var aDate, oDate1, oDate2, iDays;
+
+        aDate = sDate1.split("-")
+        oDate1 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])    //转换为12-12-2012格式
+
+        aDate = sDate2.split("-")
+        oDate2 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])
+
+        iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24)    // 把相差的毫秒数转换为天数
+
+        return iDays
+
+    },
+
+
+
+    // ------------------------ 算法相关 ---------------------------------//
+
+
+
+
+    /* 记忆函数 【 memoizer 】*/
+    memoizer: function (memo, fundamental) {
+
+        var shell = function (n) {
+
+            var result = memo[n];
+
+            if (typeof result !== "number") {
+                result = fundamental(shell, n);
+                memo[n] = result;
+            }
+
+            return result;
+
+        }
+
+        return shell;
+    },
+
+
+    /* 利用记忆函数计算阶乘 【 myBase.factorial()(5) == 120 】*/
+    factorial: function (memo, fundamental) {
+
+        var result = this.memoizer([1, 1], function (shell, n) {
+            return n * shell(n - 1);
+        })
+
+        return result;
+    },
+
+
+    /* 利用记忆函数计算斐波那契数列 【 myBase.factorial()(10) == 55 】*/
+    fibonacci: function (memo, fundamental) {
+
+        var result = this.memoizer([0, 1], function (shell, n) {
+            return shell(n - 1) + shell(n - 2);
+        })
+
+        return result;
+
+    },
+
+
+
+
+
     // ------------------------ 正则相关 ---------------------------------//
+
+
 
 
     /* 返回 str 中出现次数最多的字符 【 fineStr(str, 0, []) 】 */
@@ -259,6 +479,12 @@ var myBase = {
 
         return fineStr(s, n, fs);
 
+    },
+
+
+    /* 去除字符串两边空格 【 trim(str) 】 */
+    trim: function (str) {
+        return str.replace(/(^\s*)|(\s*$)/g, "");
     },
 
 }
